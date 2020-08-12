cd7e87e8d2fc63676dc078caca61e2cc552a0de4
Arjun Ramanathan <arjurama@cisco.com>
2020-08-12 16:57:06 -0700

    changed color scheme, finished contact for the most part and added resume tab

---
+++ static/funcs.js
@@ -0,0 +1,8 @@
+// function headerHover(x) {
+//     // Light navy: #286086
+//     x.style.backgroundColor = "#B0E0E6";
+// }
+// function headerUnhover(x) {
+//     // Mich Blue: #00274C
+//     x.style.backgroundColor = "#87CEFA"
+// }
