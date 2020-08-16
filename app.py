f25ba0f0d136b6256261dbd767e347f02f3f1a6b
Arjun Ramanathan <arjunsr@umich.edu>
2020-08-16 15:37:37 -0700

    maybe this helps

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def favicon():
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
