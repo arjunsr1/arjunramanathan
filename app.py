ee49d77c3d2710105dfdcee9e452e7bbf59d259e
Arjun Ramanathan <arjunsr@umich.edu>
2020-08-16 13:21:24 -0700

    Finished first draft of all

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
 import flask
 import os
+import random
 
 app = flask.Flask(__name__)
+
+image_link_set = dict()
+pic_indexer = 0
+
 
 @app.route('/', methods=['GET'])
 def index():
     return flask.render_template('index.html')
+
+def get_pics():
+    context={}
+    file_list = []
+    img_dir = os.path.join(app.root_path, 'static/images')
+    return os.listdir(img_dir)
+
+@app.route('/api/randomurl/', methods=['GET'])
+def get_random_pic():
+    global image_link_set
+    global pic_indexer
+    context={}
+    # if set is full, use global counter to index through set to display same pics in order.
+    if len(get_pics()) == len(image_link_set):
+        print ('set is of size ', len(image_link_set))
+        url = image_link_set[pic_indexer]
+        context['img_url'] = '/static/images/' + url
+        pic_indexer = (pic_indexer + 1) % len(image_link_set)
+        return flask.jsonify(**context)
+    # get image file names.
+    img_list = get_pics()
+    img_list_len = len(img_list)
+    temp = []
+    for fn in img_list:
+        # if set contains one, dont add it to temp array
+        if fn not in image_link_set.values():
+            temp.append(fn)
+    
+    url = random.choice(temp)
+    image_link_set[pic_indexer] = (url)
+    pic_indexer = (pic_indexer + 1) % img_list_len
+    context['img_url'] = '/static/images/' + url
+    print (context)
+    return flask.jsonify(**context)
+    # pick random image name from temp array and then add it to set
 
 @app.route('/contact/', methods=['GET'])
 def contact():
@@ -13,7 +53,34 @@ def contact():
 
 @app.route('/photos/', methods=['GET'])
 def photos():
-    return flask.render_template('photos.html')
+    global image_link_set
+    global pic_indexer
+    context={}
+    # if set is full, use global counter to index through set to display same pics in order.
+    if len(get_pics()) == len(image_link_set):
+        print ('set is of size ', len(image_link_set))
+        url = image_link_set[pic_indexer]
+        context['img_url'] = '/static/images/' + url
+        pic_indexer = (pic_indexer + 1) % len(image_link_set)
+        return flask.render_template('photos.html', **context)
+        
+    # get image file names.
+    img_list = get_pics()
+    img_list_len = len(img_list)
+    temp = []
+    for fn in img_list:
+        # if set contains one, dont add it to temp array
+        if fn not in image_link_set.values():
+            temp.append(fn)
+    
+    url = random.choice(temp)
+    image_link_set[pic_indexer] = (url)
+    pic_indexer = (pic_indexer + 1) % img_list_len
+    context['img_url'] = '/static/images/' + url
+    print (pic_indexer)
+    print ()
+    print (context)
+    return flask.render_template('photos.html', **context)
 
 @app.route('/resume/', methods=['GET'])
 def render_resume():
--- static/funcs.js
+++ static/funcs.js
@@ -6,3 +6,22 @@
 //     // Mich Blue: #00274C
 //     x.style.backgroundColor = "#87CEFA"
 // }
+
+// function change_pics() {
+//     const image_urls = await fetchImgNames();
+//     console.log(image_urls);
+//     console.log('done.');
+// }
+
+function getImagePath(){
+    fetch('/api/randomurl/', {credentials: "same-origin"})
+    .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data['img_url'])
+        document.getElementById('photo_change').src=data['img_url'];
+    })
+    .catch((error) => console.log(error));
+}
