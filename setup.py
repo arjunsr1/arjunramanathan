af72fb6ebd3c7108361875ce1ad0601dd2eb3747
Arjun Ramanathan <arjunsr@umich.edu>
2020-08-16 15:20:38 -0700

    Adding setup works?

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='arjunramanathan',
+    version='0.1.0',
+    include_package_data=True,
+    install_requires=[
+        'Flask==1.1.2',
+        'requests==2.24.0',
+    ],
+    python_requires='>=3.6',
+)
